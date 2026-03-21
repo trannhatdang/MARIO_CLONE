de9f228e81f3bd4294efa8b5b16137d62c41c528
trannhatdang <hihihibly@gmail.com>
2026-03-21 15:27:20 +0700

    camera

--- include/CustomScene/GameScene/Camera.h
+++ include/CustomScene/GameScene/Camera.h
@@ -2,14 +2,20 @@
 #define CAMERA_H_
 
 #include "engine/Components/Component.h"
+#include "engine/Components/Transform.h"
 #include "engine/main.h"
 #include "Config.h"
 
 class Camera : public Component
 {
 	private:
+		Transform* m_tfs;
+		Transform* m_player_tfs;
 		GameObject* m_player;
 		void (*m_setCameraPosFunc)(Vector3);
+
+		void moveLeft();
+		void moveRight();
 	public:
 		Camera(GameObject* obj, GameObject* player, void (*setCameraPosFunc)(Vector3));
 		~Camera();
--- src/CustomScene/GameScene/Camera.cpp
+++ src/CustomScene/GameScene/Camera.cpp
@@ -1,8 +1,9 @@
 #include "CustomScene/GameScene/Camera.h"
+#include "engine/GameObject.h"
 
 Camera::Camera(GameObject* obj, GameObject* player, void (*setCameraPosFunc)(Vector3)) : Component("Camera", obj), m_player(player), m_setCameraPosFunc(setCameraPosFunc)
 {
-
+	m_player_tfs = player->GetTransform();
 }
 
 Camera::~Camera()
@@ -10,12 +11,34 @@ Camera::~Camera()
 
 }
 
+void Camera::moveLeft()
+{
+	Vector3 camPos = GetCameraPos();
+	(*m_setCameraPosFunc)(camPos + Vector3(-1, 0, 0));
+}
+
+void Camera::moveRight()
+{
+	Vector3 camPos = GetCameraPos();
+	(*m_setCameraPosFunc)(camPos + Vector3(1, 0, 0));
+}
+
 void Camera::OnIterate()
 {
 	Vector3 camPos = GetCameraPos();
+	Vector3 playerPos = m_player_tfs->GetPosition();
 	SDL_Rect l_rect = { camPos.x, camPos.y, 100, 630 };
 	SDL_Rect r_rect = { camPos.x + 315, camPos.y, 560, 630 };
 
+	if(IsPointInsideRect(playerPos, l_rect))
+	{
+		moveLeft();
+	}
+	
+	if(IsPointInsideRect(playerPos, r_rect))
+	{
+		moveRight();
+	}
 }
 
 std::unique_ptr<Component> Camera::copy()
